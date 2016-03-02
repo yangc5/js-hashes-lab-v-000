404f4b82c3db18e111f446c435850ff37ab7f190
Chen Yang <yangc5@mail.uc.edu>
2016-03-02 18:04:02 +0000

    Done.

--- recipe.js
+++ recipe.js
@@ -1,1 +1,27 @@
 'use strict';
+
+function addIngredient(recipe, ingre, quantity){
+  recipe[ingre]=quantity;
+  return recipe;
+}
+
+function removeIngredient(recipe, ingre){
+  delete recipe[ingre];
+  return recipe;
+}
+
+function updateIngredient(recipe, ingre, quantity){
+  recipe[ingre]=quantity;
+  return recipe;
+}
+
+function readRecipe(recipe){
+  if(Object.keys(recipe).length>0){
+    for(var ingre in recipe){
+      console.log("this recipe calls for "+recipe[ingre]+" of "+ingre);
+    }
+  }
+}
+
+readRecipe({chocolate: "16 oz"});
+
